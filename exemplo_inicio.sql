88cc48ef4bdae38b0d25f91ab174c69cccdf56e0
LuwcasGbs <luc4sgbsoar3s@gmail.com>
2024-09-06 11:20:13 -0300

    :v

---
+++ exemplo_inicio.sql
@@ -0,0 +1,38 @@
+#create database empresa_14;
+#use empresa_1e;
+
+#create table cliente(
+#codigo integer,
+#nome varchar(50),
+#endereco varchar(50)
+#);
+
+#describe cliente;
+
+#reate table produto(
+#cod integer,
+#descricao varchar(40),
+#valor float
+#);
+
+#describe produto;
+#DROP TABLE cliente;
+#DROP TABLE produto;
+
+#CREATE TABLE produtos(
+#cod_prod integer PRIMARY KEY,
+#nome varchar(40),
+#preco numeric(10,2)
+#);
+
+#describe produtos;
+
+#CREATE TABLE exemplo(
+#cod integer,
+#item integer,
+#cod_prod integer,
+#PRIMARY KEY (cod,item)
+#);
+
+#describe exemplo;
+
---
+++ exercicio1.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE bd_teste_1e;
+use bd_teste_1e;
+
+create table tbusuarios(
+iduser integer PRIMARY KEY,
+usuario varchar(50),
+login varchar(20),
+senha varchar(20),
+perfil varchar(20)
+);
+
+create table pessoa(
+id integer PRIMARY KEY,
+bi varchar(45),
+nome varchar(45),
+data_nasc date,
+telefone varchar(45),
+email varchar(45),
+morada varchar(45)
+);
+
+create table tbfuncionarios(
+idfunc integer,
+nome varchar(50),
+registro integer PRIMARY KEY,
+guerra varchar(30),
+gerencia varchar(20),
+det varchar(20),
+turno varchar(10),
+cargo varchar(30),
+atividade varchar(50),
+funcao varchar(50),
+vinculo varchar(30),
+situacao varchar(30)
+);
+
+describe pessoa;
+describe tbfuncionarios;
+describe tbusuarios;
